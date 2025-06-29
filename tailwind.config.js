bf5f923efb4847100a2fd7b4e4d26df3ec04d396
Agnyathavasi <vishwajithhh12345@gmail.com>
2025-06-29 18:39:05 +0530

    Added deployments configs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Tailwind scans all JSX/TSX files in /src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
